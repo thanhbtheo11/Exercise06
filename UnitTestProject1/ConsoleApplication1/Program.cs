7acdd7caa1aa555d87d3c4df0bd56e98ddd388cc
thanhbtheo11 <thanhbtheo11@gmail.com>
2019-10-17 15:03:31 +0700

    1

---
+++ UnitTestProject1/ConsoleApplication1/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication1
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("money bank");
+            int money = int.Parse(Console.ReadLine());
+            int c, d;
+            Console.WriteLine("money with");
+            int with = int.Parse(Console.ReadLine());
+            c = money - with;
+            Console.WriteLine("Balance " + money + "-" + with + "=" + c);
+            Console.WriteLine("money bank " + c);
+            Console.WriteLine("money Depo ");
+            int Depo = int.Parse(Console.ReadLine());
+            d = c + Depo;
+            Console.WriteLine("Balance " + c + "+" + Depo + "=" + d);
+        }
+
+    }
+}
---
+++ UnitTestProject1/UnitTestProject1/UnitTest1.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using ConsoleApplication1;
+
+namespace UnitTestAccount
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            Account acc = new Account(200000);
+
+            Assert.IsNotNull(acc);
+            Assert.IsInstanceOfType(acc, typeof(Account));
+            Assert.AreEqual(200000, acc.Balance());
+        }
+        [TestMethod]
+
+        public void TestMethodDeposit()
+        {
+            Account acc = new Account(500000);
+            acc.Deposit(100000);
+            Assert.AreEqual(600000, acc.Balance());
+            acc.Deposit(100000);
+            Assert.AreEqual(700000, acc.Balance());
+        }
+        [TestMethod]
+
+        public void TestMethodWithdraw()
+        {
+            Account acc = new Account(300000);
+            acc.Deposit(100000);
+            Assert.AreEqual(400000, acc.Balance());
+            acc.Deposit(100000);
+            Assert.AreEqual(500000, acc.Balance());
+        }
+        [TestMethod]
+
+        public void TestMethod()
+        {
+            Account acc = new Account(300000);
+            acc.Deposit(100000);
+            Assert.AreEqual(400000, acc.Balance());
+            acc.Deposit(100000);
+            Assert.AreEqual(500000, acc.Balance());
+        }
+    }
+}
